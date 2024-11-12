a1bd87e76138f2544d33019cda959e4ae4e5c416
Crowdbotics <team@crowdbotics.com>
2024-11-12 06:03:24 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dream-weaver-49993",
+  url: "https://dream-weaver-49993.botics.co",
+  api: "https://dream-weaver-49993.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
